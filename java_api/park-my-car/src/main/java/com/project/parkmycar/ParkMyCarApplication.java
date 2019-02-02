6b2c1846ebb38df3978ac80feaad22959e3a9236
Vanessa Pinto <vanessapinto257@gmail.com>
2019-02-02 01:58:46 -0500

    #5 login-registration first draft

---
+++ java_api/park-my-car/src/main/java/com/project/parkmycar/ParkMyCarApplication.java
@@ -0,0 +1,16 @@
+package com.project.parkmycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ParkMyCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkMyCarApplication.class, args);
+	}
+
+}
+
