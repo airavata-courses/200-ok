3caef39393a69046e4972a75782c633b32fc1155
shubham1812 <sgodshalwar@gmail.com>
2019-02-04 19:21:56 -0500

    select item on home page

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,6 +4,7 @@ import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
 
 
 
@@ -11,6 +12,7 @@ export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
